7f78dfea733e020a078696035a8595e55f62c017
Parth Mehta <parth@techizer.in>
2020-12-15 16:40:06 +0530

    + list item created + Diffutil class created

---
+++ app/src/main/java/com/motion/recyclerviewdiffutils/MyDiffUtilCallback.kt
@@ -0,0 +1,22 @@
+package com.motion.recyclerviewdiffutils
+
+import androidx.recyclerview.widget.DiffUtil
+
+class MyDiffUtilCallback(val oldList: List<String>, val newList: List<String>) :
+    DiffUtil.Callback() {
+    override fun getOldListSize(): Int {
+        return oldList.size
+    }
+
+    override fun getNewListSize(): Int {
+        return newList.size
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldItemPosition == newItemPosition
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition] == newList[newItemPosition]
+    }
+}
